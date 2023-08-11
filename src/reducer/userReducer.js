56294783df3ace2bfd4243ceb2f4e0e384b3234d
José Henao <josexdjose14@gmail.com>
2023-08-11 11:50:39 -0500

    first commit publish feature

--- src/reducer/userReducer.js
+++ src/reducer/userReducer.js
@@ -1,23 +1,23 @@
 export const initialUser = {
-    isAuthenticated: false,
-    user: null
+  isAuthenticated: true,
+  user: null
 }
 
 export const userReducer = (state = initialUser, action) => {
-    switch (action.type) {
-        case "login":
-            return action.payload;
-        case "Logout":
-            return initialUser;
-        case "update":
-            return {
-                ...state,
-                user: {
-                    ...state.user,
-                    ...action.payload
-                }
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case "login":
+      return action.payload;
+    case "Logout":
+      return initialUser;
+    case "update":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          ...action.payload
+        }
+      };
+    default:
+      return state;
+  }
 }
