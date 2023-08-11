ce58d5d8b454bfaf149ac3d75923cc48d884e42c
mrcsbda <macbedoya@gmail.com>
2023-08-11 10:38:31 -0500

    add endpoints

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Footer from '../../components/footer/footer'
+import "./layout.scss"
 
 const Layout = () => {
     return (
---
+++ src/services/data.js
@@ -0,0 +1,7 @@
+export const URL_API = "https://backend-socialnetwork.onrender.com/"
+
+export const endpoints = {
+    user: `${URL_API}users`,
+    posts: `${URL_API}posts`,
+    comments: `${URL_API}comments`
+}
