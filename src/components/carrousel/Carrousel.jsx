0e7a3dadc769ca9933ed96678d70ec1c7338131d
mrcsbda <macbedoya@gmail.com>
2023-08-11 16:45:11 -0500

    change carrousel

--- src/components/carrousel/Carrousel.jsx
+++ src/components/carrousel/Carrousel.jsx
@@ -1,11 +1,42 @@
-import React, { useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import './carrousel.scss';
 import { Pagination } from 'swiper/modules';
+import { getUsers } from '../../services/userServices';
+import { getSession } from '../../services/storageService';
 
-const Carrousel = ({ infoHeader }) => {
+const Carrousel = () => {
+
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    getDate()
+  }, [])
+
+  const getDate = async () => {
+    const data = await getUsers()
+    const userLogged = getSession()
+    const usersInfo = [
+      {
+        avatar: userLogged.avatar,
+        name: "Tu historia"
+      }
+    ]
+
+    data.forEach(user => {
+      if (user.id !== userLogged.id) {
+        usersInfo.push({
+          avatar: user.avatar,
+          name: user.name,
+        })
+      }
+    })
+
+    setUsers(usersInfo)
+  }
+
   return (
     <>
       <Swiper
@@ -15,8 +46,8 @@ const Carrousel = ({ infoHeader }) => {
         className="mySwiper header__carousel-container"
       >
         {
-          infoHeader.length && (
-            infoHeader.map((user, index) => (
+          users.length && (
+            users.map((user, index) => (
               <SwiperSlide className="header__slide" key={index}>
                 <figure className="header__avatar-container">
                   <img className="header__avatar" src={user.avatar} alt={user.name} />
--- src/services/data.js
+++ src/services/data.js
@@ -4,6 +4,7 @@ export const endpoints = {
     users: `${URL_API}users`,
     posts: `${URL_API}posts`,
     comments: `${URL_API}comments`,
+    usersWithPosts: `${URL_API}users?_embed=posts`,
     user: (username, password)=> {
         return `${URL_API}users?username=${username}&password=${password}`
     }
