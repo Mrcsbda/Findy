e2d252f7bd3ff49e0b4837327e6658b0e1544002
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-11 11:26:10 -0500

    mend

--- src/services/borramedespues.js
+++ src/services/borramedespues.js
@@ -1,5 +1,17 @@
 import { endpoints } from "./data";
+import axios from "axios";
 
-const get_users = async() =>{
-    const {data} = axios
+export const get_users = async(id) =>{
+    const {data} = axios.get(`${endpoints.user} / ${id}`)
+    return data
+}
+
+export const get_Posts = async(idUser) =>{
+    const {data} = axios.get(`${endpoints.posts}?userId=${idUser}`)
+    return data
+}
+
+export const get_Message = async(postId) =>{
+    const {data} = axios.get(`${endpoints.comments}?postId=${postId}`)
+    return data
 }
