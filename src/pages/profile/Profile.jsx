24217b777344ac1c49472f32150b0ab5443cc7c6
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-11 12:15:18 -0500

    mend

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -3,15 +3,27 @@ import "./profile.scss"
 
 const Profile = () => {
   useEffect(() => {
- 
+
   }, [])
-  
+
 
   return (
-    <div>
+    <section>
       <div className='profile__portrait'><figure>Hola</figure></div>
+      <div>
+        <article>
+          <div>10.7 M</div>
+          <figure><img src="" alt="Userimg" /></figure>
+          <div>108.3 M</div>
+        </article>
+        <div>
+          <p>Jennie Kim</p>
+          <p>Usuario1</p>
+          <p></p>
 
-    </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
