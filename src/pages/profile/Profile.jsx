ddc1a5fb61f98ac3147481c7a86da479fb50728c
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-11 14:11:03 -0500

    mend

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -9,20 +9,28 @@ const Profile = () => {
 
   return (
     <section>
-      <div className='profile__portrait'><figure>Hola</figure></div>
-      <div>
-        <article>
+      <section className='profile__portrait'>
+        <figure className='profile_backButton'><img src="/images/arrow-left.svg" alt="arrow-left" /></figure>
+        <figure><img src="" alt="user_portrait"/></figure>
+        <figure><img src="/images/ellipsis.svg" alt="ellipsis.svg" /></figure>
+        </section>
+      <section>
+        <article className='profile__stats'>
           <div>10.7 M</div>
           <figure><img src="" alt="Userimg" /></figure>
           <div>108.3 M</div>
         </article>
-        <div>
-          <p>Jennie Kim</p>
-          <p>Usuario1</p>
-          <p></p>
-
+        <div className='profile__inf'>
+          <p className='profile__inf__name'>Jennie Kim</p>
+          <p className='profile__inf__username'>Usuario1</p>
+          <p className='profile__inf__description'>Decripcion</p>
         </div>
-      </div>
+        <div className='profile__buttons'>
+          <div className='profile__button'>Follow</div>
+          <div className='profile__button'>Messages</div>
+        </div>
+      </section>
+
     </section>
   )
 }
