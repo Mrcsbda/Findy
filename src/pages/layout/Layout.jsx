0d44f982813b8f122a122ce12cd434034abaf375
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-11 10:46:27 -0500

    Capital case Footer

--- src/pages/layout/Layout.jsx
+++ src/pages/layout/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import Footer from '../../components/footer/footer'
+import Footer from '../../components/footer/Footer'
 import "./layout.scss"
 
 const Layout = () => {
