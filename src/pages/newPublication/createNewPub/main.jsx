9d6e2d5186d16cababe43f8b34ef73eacbb18784
José Henao <josexdjose14@gmail.com>
2023-08-12 00:14:16 -0500

    save v1

--- src/pages/newPublication/createNewPub/main.jsx
+++ src/pages/newPublication/createNewPub/main.jsx
@@ -4,17 +4,21 @@ const CreateNewPub = () => {
   return (
     <>
       <form onSubmit={handleSubmit(onSubmit)} className='form' >
-        <div className='form__type'>
-          <label htmlFor="pubType">imagen12345</label>
-          <input type="url" placeholder='Ingrese la url del contenido' {...register("pubType")} />
+        <div className='form__box'>
+          <label htmlFor="pubType">Agregar ****</label>
+          <input type="url" {...register("pubType")} />
         </div>
-        <div className='form__type'>
-          <label htmlFor="pubType">Correo electronico</label>
-          <input type="url"  {...register("pubType")} />
+        <div className='form__box'>
+          <label htmlFor="pubText">Agregar descripcion</label>
+          <input type="text"  {...register("pubText")} />
         </div>
-        <div className='form__type'>
-          <label htmlFor="pubType">Correo electronico</label>
-          <input type="url"  {...register("pubType")} />
+        <div className='form__box'>
+          <label htmlFor="pubTags">Etiquetar personas</label>
+          <input type="text"  {...register("pubTags")} />
+        </div>
+        <div className='form__box'>
+          <label htmlFor="pubTopics">Agregar temas</label>
+          <input type="text"  {...register("pubTopics")} />
         </div>
       </form>
     </>
