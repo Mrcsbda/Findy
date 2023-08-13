b01127c69749346853470c52c005004167e28cd6
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-12 21:09:59 -0500

    Mansory implementation

--- src/pages/profile/images/images.jsx
+++ src/pages/profile/images/images.jsx
@@ -1,22 +1,7 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import { styled } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
 import { Masonry } from '@mui/lab';
 
-
-// const Item = styled(Paper)(({ theme }) => ({
-//  // backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
-//  // ...theme.typography.body2,
-//  // padding: theme.spacing(0.5),
-// // textAlign: 'center',
-//  // color: theme.palette.text.secondary,
-// }));
-
 export default function ImagesProfile({ posts }) {
-
-
-
 
     return (
         <Masonry columns={2} spacing={1} className='profile__posts' sx={{}}>
