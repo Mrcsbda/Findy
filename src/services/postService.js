f0d05d0369f6d4cc55c7eeeff23d6b44285d7574
José Henao <josexdjose14@gmail.com>
2023-08-12 19:47:05 -0500

    new publication feature incomplete

--- src/services/postService.js
+++ src/services/postService.js
@@ -2,10 +2,19 @@ import axios from "axios"
 import { endpoints } from "./data"
 
 export const getPosts = async (idUser) => {
-    try {
-        const { data } = await axios.get(`${endpoints.posts}?userId=${idUser}`)
-        return data
-    } catch (error) {
-        return null
-    }
+  try {
+    const { data } = await axios.get(`${endpoints.posts}?userId=${idUser}`)
+    return data
+  } catch (error) {
+    return null
+  }
+}
+
+export const postPost = async (postCont) => {
+  try {
+    const { status } = await axios.post(endpoints.posts, postCont)
+    return status
+  } catch (error) {
+    return null
+  }
 }
