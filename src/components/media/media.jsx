896b285bcf33330ac4136936d9c4306032770798
Sebastian Ruiz <sebasruiz15@gmail.com>
2023-08-14 19:17:44 -0500

    mend

--- src/components/media/media.jsx
+++ src/components/media/media.jsx
@@ -7,41 +7,44 @@ export default function PostsProfile({ posts, typeMedia, idUser }) {
 
 
     React.useEffect(() => {
-        const handleTypeMedia = async ()=> {
-        switch (typeMedia) {
-            case "photos":
-               const photosPosts = posts.filter(post => post.type && post.type === "photo")
-               setArrayPosts(photosPosts)
-              break;
-            case "videos":
-                const videosPosts = posts.filter(post => post.type && post.type === "video")
-                setArrayPosts(videosPosts)
-              break;
-            case "tag":
-                const taggedPosts = await getTaggedPost(idUser)
-                setArrayPosts(taggedPosts)
-              break;
-          }
+        const handleTypeMedia = async () => {
+            switch (typeMedia) {
+                case "photos":
+                    const photosPosts = posts.filter(post => post.type && post.type === "photo")
+                    setArrayPosts(photosPosts)
+                    break;
+                case "videos":
+                    const videosPosts = posts.filter(post => post.type && post.type === "video")
+                    setArrayPosts(videosPosts)
+                    break;
+                case "tag":
+                    const taggedPosts = await getTaggedPost(idUser)
+                    setArrayPosts(taggedPosts)
+                    break;
+            }
         }
         handleTypeMedia()
     }, [typeMedia])
-    
+
 
 
     return (
         <Masonry columns={2} spacing={1} className='profile__posts' sx={{}}>
             {
-            arrayPosts.length>0 ?
-            arrayPosts.map((post) => (
-
-                <figure key={post.id} data-id={post.id} className='profile__posts__post'>
-                    <img src={post?.media} alt="post_media" />
-                </figure>
-
-            ))
-                :
-                <div>No hay posts</div>
-        }
+                arrayPosts.length > 0 ?
+                    arrayPosts.map((post) => (
+                        post.type == "photo" ?
+                            <figure key={post.id} data-id={post.id} className='profile__posts__post'>
+                                <img src={post?.media} alt="post_media" />
+                            </figure>
+                            :
+                            <figure key={post.id} data-id={post.id} className='profile__posts__post'>
+                                <iframe src={post?.media} frameborder="0" allowfullscreen></iframe>
+                            </figure>
+                    ))
+                    :
+                    <div>No hay posts</div>
+            }
         </Masonry>
     );
 }
